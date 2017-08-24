68c7f970d7f3f81804781b04535a65aa0faf34fa
Lou Franklin <franklin@thoughtworks.com>
2017-08-24 17:09:19 +0100

    simple cycle detection

---
+++ src/CycleDetectionDFS/CycleDetection.java
@@ -0,0 +1,33 @@
+package CycleDetectionDFS;
+
+import java.util.List;
+import java.util.Stack;
+
+public class CycleDetection {
+
+    public void detectCycles(List<Vertex> list) {
+
+        for (Vertex v : list) {
+            if (!v.isVisited()) {
+                dfs(v);
+            }
+        }
+    }
+
+    private void dfs(Vertex vertex) {
+        vertex.setBeingVistited(true);
+        for (Vertex v : vertex.getNeighbourList()) {
+            if (v.isBeingVistited()) {
+                System.out.print("Backward edge, so there is a cycle!");
+                return;
+            }
+            if (!v.isVisited()) {
+                v.setBeingVistited(true);
+                dfs(v);
+            }
+        }
+        vertex.setBeingVistited(false);
+        vertex.setVisited(true);
+    }
+
+}
---
+++ src/CycleDetectionDFS/Vertex.java
@@ -0,0 +1,58 @@
+package CycleDetectionDFS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+
+    private String name;
+    private boolean visited;
+    private boolean beingVistited;
+    private List<Vertex> neighbourList;
+
+    public Vertex(String name) {
+        this.name = name;
+        this.neighbourList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean isBeingVistited() {
+        return beingVistited;
+    }
+
+    public void setBeingVistited(boolean beingVistited) {
+        this.beingVistited = beingVistited;
+    }
+
+    public List<Vertex> getNeighbourList() {
+        return neighbourList;
+    }
+
+    public void setNeighbourList(List<Vertex> neighbourList) {
+        this.neighbourList = neighbourList;
+    }
+
+    public void addNeighbour(Vertex vertex) {
+        this.neighbourList.add(vertex);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}

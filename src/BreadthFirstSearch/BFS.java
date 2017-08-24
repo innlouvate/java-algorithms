07bc9f0e4f2c1b20c60e30600fe345c8faf77280
Lou Franklin <franklin@thoughtworks.com>
2017-08-24 11:38:10 +0100

    basic breadth first search

---
+++ src/BreadthFirstSearch/BFS.java
@@ -0,0 +1,28 @@
+package BreadthFirstSearch;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public void bfs(Vertex root) {
+
+        Queue<Vertex> queue = new LinkedList<>();
+        root.setVisited(true);
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            Vertex vertex = queue.remove();
+            System.out.println(vertex.getData()+"  ");
+
+            for (Vertex v : vertex.getNeighbourList()) {
+                if(!v.isVisited()) {
+                    v.setVisited(true);
+                    queue.add(v);
+                }
+            }
+        }
+
+    }
+
+}
